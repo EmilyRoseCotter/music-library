5c099dc7d2f228e92cd648413225324c9fb1988b
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2022-02-15 18:48:34 +0000

    finished initial set up

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+
 const app = express();
-app.use(express.json);
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    return res.status(200).json({ result: 'Hello world'});
+});
 
 module.exports = app;
